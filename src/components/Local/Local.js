b7e90fac7baa4576a190ee9c1f09f2fad91322e3
NiakaGaming <niaka-ishigame@hotmail.com>
2020-10-20 11:30:18 +0200

    components created

---
+++ src/components/Local/Local.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Local extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Local;
