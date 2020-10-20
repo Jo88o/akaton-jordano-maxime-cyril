3656cd8ea82e677043eac7b31f29aceddeb2d2e3
NiakaGaming <niaka-ishigame@hotmail.com>
2020-10-20 17:02:24 +0200

    Sub Form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import NavBar from "./components/NavBar/NavBar"
 import Local from "./components/Local/Local"
 import Abonnement from "./components/Abonnement/Abonnement"
 import React, { Component } from 'react';
+import "./App.css"
 
 class App extends Component {
+  state = {
+    abo: false
+  }
   render() {
     return (
       <div>
         <NavBar />
         <Local />
-        <Abonnement />
+        <Abonnement info={this.state} />
       </div>
     );
   }
