38bb6709edb3aba82e083795d32afd1113dd4eb6
NiakaGaming <niaka-ishigame@hotmail.com>
2020-10-20 11:35:37 +0200

    public remove unused files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
 import NavBar from "./components/NavBar/NavBar"
 import Local from "./components/Local/Local"
 import Abonnement from "./components/Abonnement/Abonnement"
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Local />
-      <Abonnement />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Local />
+        <Abonnement />
+      </div>
+    );
+  }
 }
 
 export default App;
