acc37bf78239888744c505047df71340d2f8efe7
cyril.haubourdin@gmail.com <cyril.haubourdin@gmail.com>
2020-10-21 09:09:13 +0200

    Navbar entamé

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import "./Navbar.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                
+                <nav className="bg">
+                    <h1 className="text-center">Hakathon</h1>
+                    <ul>
+                        <li>Local</li>
+                        <li>Abonnements</li>
+                    </ul>
+                </nav>
             </div>
         );
     }
 }
 
 export default NavBar;
+
+//COULEURS : VERT (#5dd55d)
+//           BLEU (#4d79FF)
